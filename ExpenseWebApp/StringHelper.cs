6f6c0e5d2ad02babc79f3c90d4bc562df7fdf4f9
Abdel Raoof <olakara@gmail.com>
2025-04-26 15:54:07 +0400

    feat: implement data export functionality (csv file)

---
+++ ExpenseWebApp/StringHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace ExpenseWebApp;
+
+public static class StringHelper
+{
+    public static string EscapeCsvField(string field)
+    {
+        if (field.Contains(",") || field.Contains("\"") || field.Contains("\n"))
+        {
+            return $"\"{field.Replace("\"", "\"\"")}\"";
+        }
+        return field;
+    }
+}
